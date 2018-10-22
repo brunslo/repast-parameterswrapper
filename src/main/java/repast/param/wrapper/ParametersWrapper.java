6cdeea4326fd94a91b5c7994b519411512e49faf
Loren Bruns <lorenbruns@gmail.com>
2018-10-22 13:35:10 +1100

    Clean up code-style

--- src/main/java/repast/param/wrapper/ParametersWrapper.java
+++ src/main/java/repast/param/wrapper/ParametersWrapper.java
@@ -6,37 +6,38 @@ import repast.simphony.parameter.Parameters;
 import java.io.IOException;
 
 public final class ParametersWrapper {
-	private static boolean USE_WEBCLIENT;
+    private static boolean USE_WEBCLIENT;
 
-	private static ParametersWrapper instance;
-	private static  String ENDPOINT_URL;
+    private static ParametersWrapper instance;
 
-	private ParametersWrapper() {
-	}
+    private static String ENDPOINT_URL;
 
-	public static ParametersWrapper getInstance() {
-		if (instance == null) {
-			instance = new ParametersWrapper();
-		}
+    private ParametersWrapper() {
+    }
 
-		return instance;
-	}
+    public static ParametersWrapper getInstance() {
+        if (instance == null) {
+            instance = new ParametersWrapper();
+        }
 
-	public void setEndPointURL(String _url) {
-		ENDPOINT_URL= _url;
-	}
+        return instance;
+    }
 
-	public void setWebClient(Boolean input) {
-		USE_WEBCLIENT=input;
-	}
-	public Parameters getParameters() throws IOException {
-		return USE_WEBCLIENT ? WebclientParameters.receiveParam(ENDPOINT_URL) : RunEnvironment.getInstance().getParameters();
-	}
+    public void setEndPointURL(String _url) {
+        ENDPOINT_URL = _url;
+    }
 
-	public void setParameters() throws IOException {
-		if(USE_WEBCLIENT)
-		{
-			WebclientParameters.transmitParam(ENDPOINT_URL);
-		}
-	}
+    public void setWebClient(Boolean input) {
+        USE_WEBCLIENT = input;
+    }
+
+    public Parameters getParameters() throws IOException {
+        return USE_WEBCLIENT ? WebclientParameters.receiveParam(ENDPOINT_URL) : RunEnvironment.getInstance().getParameters();
+    }
+
+    public void setParameters() throws IOException {
+        if (USE_WEBCLIENT) {
+            WebclientParameters.transmitParam(ENDPOINT_URL);
+        }
+    }
 }
